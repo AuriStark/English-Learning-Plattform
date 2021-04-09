0415e4c1eeb4d797de5deb1d2cbb4cb187247ef6
Aurelle Wafo <59320937+AuriStark@users.noreply.github.com>
2021-04-09 10:30:02 +0200

    Some Vuetify API

--- platform-code/src/App.vue
+++ platform-code/src/App.vue
@@ -1,58 +1,28 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <!-- 
+      Purple 
+      Purple = #800080
+      Fuchsia =	#FF00FF
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+      or This One  PS: Check Out Purple Palette On Vuetify
+      A = #461A42
+      B = #623C5E
+    -->
     <v-main>
-      <HelloWorld/>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
   },
-
   data: () => ({
     //
   }),
--- platform-code/src/views/Home.vue
+++ platform-code/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Homepage</h1>
+    <p class ="red white--text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci architecto natus reiciendis sed molestias saepe.</p>
+    <p class ="pink lighten-4 red--text text--darken-4"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci architecto natus reiciendis sed molestias saepe.</p>
+    <h1 class="display-4"> Massive Display </h1>
+    <h4 class="display-1"> Smaller Display </h4>
+    <p class="Headline"> This is a Headline </p>
+    <p class="Subheading"> A Subheading </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
