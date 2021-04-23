446049967056c199a9e26b35d2b06d94a045ace6
Aurelle Wafo <59320937+AuriStark@users.noreply.github.com>
2021-04-23 12:54:10 +0200

    Added a NavBar

--- training-code/src/components/Navbar.vue
+++ training-code/src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
     <nav>
         <v-toolbar flat app>
+            <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer">
+            </v-app-bar-nav-icon>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Todo</span>
                 <span>App</span>
@@ -11,13 +13,33 @@
                 <v-icon right class="mdi mdi-logout-variant"> </v-icon>
             </v-btn>
         </v-toolbar>
+
+        <v-navigation-drawer app v-model="drawer">
+            <v-list-item>
+                <v-list-item-content >
+                    <v-list-item-title class="title">
+                        Application
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        TodoApp
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+        </v-navigation-drawer>
         <p> Hello </p>
     </nav>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            drawer: false
+        }
+    }
 }
 </script>
 
